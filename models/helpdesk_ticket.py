a7ecd144eb859cb79528482b21e6898881f90279
sergei.kataev <sergei.kataev@simbirsoft.com>
2022-03-23 14:00:52 +0300

    issues fix

--- models/helpdesk_ticket.py
+++ models/helpdesk_ticket.py
@@ -8,23 +8,36 @@ class HelpdeskTicket(models.Model):
 
     @api.model
     def create(self, vals):
+        """
+        overrides create to add to new record actual ticket_number value from ir.sequence
+        """
         if vals.get('ticket_number', 'New') == 'New':
             vals['ticket_number'] = self.env['ir.sequence'].next_by_code('helpdesk.ticket.number') or 'New'
         return super(HelpdeskTicket, self).create(vals)
 
     def name_get(self):
+        """
+        overrides name_get to achieve next name form:
+        [tiket_number] name
+        """
         names = []
         for ticket in self:
-            names.append((ticket.id, "[{0}] - {1}".format(ticket.ticket_number, ticket.name)))
+            names.append((ticket.id, "[{0}] {1}".format(ticket.ticket_number, ticket.name)))
         return names
 
     def name_search(self, name='', args=None, operator='ilike', limit=100):
-        args = list(args or [])
+        """
+        overrides name_search to add ticket_number in search set
+        """
+        args = args or []
         domain = ['|', ('name', 'ilike', name), ('ticket_number', 'ilike', name)]
         return super(HelpdeskTicket, self).search(args=args + domain, limit=limit).get_name()
 
     @api.model
     def init_hook(self):
+        """
+        create ticket_number for existing helpdesk.ticket records
+        """
         ticket_number = 1
         for rec in self.search([], order='id asc'):
             rec.ticket_number = ticket_number
